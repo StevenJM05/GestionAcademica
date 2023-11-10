013b48c0b51887f33742c4608ad025cf1600d3ec
ChrisAbraham07 <chris.cartagena29@gmail.com>
2023-11-10 12:28:39 -0600

    Clases terminadas de responsable

---
+++ GestionAcademica/modelos/Responsable/DatosContactosResponsable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionAcademica.modelos.Responsable
+{
+    internal class DatosContactosResponsable
+    {
+        private int idContactoRes;
+        private string telefonoPropio;
+        private string telefonoFijo;
+        private string telefonoTrabajo;
+        private string otroTelefono;
+        private int dui;
+
+        public int IdContactoRes1 { get => idContactoRes; set => idContactoRes = value; }
+        public string TelefonoPropio1 { get => telefonoPropio; set => telefonoPropio = value; }
+        public string TelefonoFijo1 { get => telefonoFijo; set => telefonoFijo = value; }
+        public string TelefonoTrabajo1 { get => telefonoTrabajo; set => telefonoTrabajo = value; }
+        public string OtroTelefono1 { get => otroTelefono; set => otroTelefono = value; }
+        public int Dui1 { get => dui; set => dui = value; }
+
+    }
+}
---
+++ GestionAcademica/modelos/Responsable/DatosPersonalesResponsable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionAcademica.modelos.Responsable
+{
+    internal class DatosPersonalesResponsable
+    {
+        private int idPersonalRes;
+        private int dui;
+        private string primerNombre;
+        private string segundoNombre;
+        private string tercerNombre;
+        private string primerApellido;
+        private string segundoApellido;
+        private string tercerApellido;
+        private string fechaNacimiento;
+        private string residencia;
+        private string direccion;
+        private string nacionalidad;
+        private string departamentoMunicipioNacimiento;
+
+        public int IdPersonalRes { get => idPersonalRes; set => idPersonalRes = value; }
+        public int Dui { get => dui; set => dui = value; }
+        public string PrimerNombre { get => primerNombre; set => primerNombre = value; }
+        public string SegundoNombre { get => segundoNombre; set => segundoNombre = value; }
+        public string TercerNombre { get => tercerNombre; set => tercerNombre = value; }
+        public string PrimerApellido { get => primerApellido; set => primerApellido = value; }
+        public string SegundoApellido { get => segundoApellido; set => segundoApellido = value; }
+        public string TercerApellido { get => tercerApellido; set => tercerApellido = value; }
+        public string FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public string Residencia { get => residencia; set => residencia = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Nacionalidad { get => nacionalidad; set => nacionalidad = value; }
+        public string DepartamentoMunicipioNacimiento { get => departamentoMunicipioNacimiento; set => departamentoMunicipioNacimiento = value; }
+    }
+}
