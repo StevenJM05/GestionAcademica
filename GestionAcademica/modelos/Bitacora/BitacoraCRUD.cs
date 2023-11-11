44e61badfb8e06bd110e03285a401cccbc0a94ad
ChrisAbraham07 <chris.cartagena29@gmail.com>
2023-11-10 18:34:36 -0600

    Clase Crud

---
+++ GestionAcademica/modelos/Bitacora/BitacoraCRUD.cs
@@ -0,0 +1,17 @@
+﻿using GestionAcademica.Clases;
+using GestionAcademica.modelos.Alumno;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionAcademica.modelos.Bitacora
+{
+    internal class BitacoraCRUD
+    {
+        ConexionBD conexionBD = new ConexionBD();
+      
+    }
+}
